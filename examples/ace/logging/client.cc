Dayrow90/muduo

examples to cpp11

examples/ace/logging/client.cc
@@ -8,8 +8,6 @@
 #include <muduo/net/TcpClient.h>
 #include <muduo/net/protobuf/codec.h>
 
-#include <boost/bind.hpp>
-
 #include <iostream>
 #include <stdio.h>
 
@@ -29,12 +27,12 @@ class LogClient : boost::noncopyable
     : client_(loop, serverAddr, "LogClient"),
       codec_(&LogRecord::default_instance(),
              "LOG0",
-             boost::bind(&LogClient::onMessage, this, _1, _2, _3))
+             std::bind(&LogClient::onMessage, this, _1, _2, _3))
   {
     client_.setConnectionCallback(
-        boost::bind(&LogClient::onConnection, this, _1));
+        std::bind(&LogClient::onConnection, this, _1));
     client_.setMessageCallback(
-        boost::bind(&ProtobufCodec::onMessage, &codec_, _1, _2, _3));
+        std::bind(&ProtobufCodec::onMessage, &codec_, _1, _2, _3));
     client_.enableRetry();
   }
 

zouzl/muduo-learning

examples/ace/logging/client.cc
@@ -8,8 +8,6 @@
 #include <muduo/net/TcpClient.h>
 #include <muduo/net/protobuf/ProtobufCodecLite.h>
 
-#include <boost/bind.hpp>
-
 #include <iostream>
 #include <stdio.h>
 
@@ -29,12 +27,12 @@ class LogClient : boost::noncopyable
  public:
   LogClient(EventLoop* loop, const InetAddress& serverAddr)
     : client_(loop, serverAddr, "LogClient"),
-      codec_(boost::bind(&LogClient::onMessage, this, _1, _2, _3))
+      codec_(std::bind(&LogClient::onMessage, this, _1, _2, _3))
   {
     client_.setConnectionCallback(
-        boost::bind(&LogClient::onConnection, this, _1));
+        std::bind(&LogClient::onConnection, this, _1));
     client_.setMessageCallback(
-        boost::bind(&Codec::onMessage, &codec_, _1, _2, _3));
+        std::bind(&Codec::onMessage, &codec_, _1, _2, _3));
     client_.enableRetry();
   }
 